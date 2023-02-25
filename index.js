df0cd62d8079763248b53b38d1184359bb33275e
avvicky <63693675+avvicky@users.noreply.github.com>
2023-02-25 20:54:18 +0530

    bug fix

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.ACCESSCAO); // Update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 app.set("viewengine", "ejs");
 app.use(express.static("public"));
 
