ab31f8905cc58f7b6c01b00829e748ff17708f32
avvicky <63693675+avvicky@users.noreply.github.com>
2023-02-25 20:57:52 +0530

    bug fix

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.ACCESSCAO); // Update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.set("viewengine", "ejs");
 app.use(express.static("public"));
 
@@ -138,6 +146,12 @@ app.get("/exercises/listbytargetmuscle/:muscle", function (req, res) {
   );
 });
 
-app.listen(3000, function () {
+var port = process.env.PORT;
+
+if (port == null) {
+  port = 4000;
+}
+
+app.listen(port, function () {
   console.log("server started on port 4000");
 });
