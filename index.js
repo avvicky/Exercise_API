87ad4c8ad13e4cee5bf36358252e21cd1813e412
avvicky <63693675+avvicky@users.noreply.github.com>
2023-02-25 18:46:28 +0530

    add cors

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const express = require("express");
 const bodParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 app.set("viewengine", "ejs");
 app.use(express.static("public"));
 
